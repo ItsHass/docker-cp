68505293d6eacc1a025feb27f467963f13c4f9dd
ItsHass <88739447+ItsHass@users.noreply.github.com>
2023-10-18 13:47:10 +0100

    Update updateRunUpdate.php

--- site/cron/updateRunUpdate.php
+++ site/cron/updateRunUpdate.php
@@ -1,9 +1,10 @@
 <?php
+require_once("../db/config.php");
 $serverIP = "192.168.0.111";
 $serverUser = "hass";
 $serverPW = "Hassan";
 
-require '/home/hass/web/svr01.itshass.uk/public_html/docker/assets/vendor/autoload.php';
+require "$site_path/assets/vendor/autoload.php";
 use phpseclib3\Net\SSH2;
 
 $ssh = new SSH2("$serverIP");
@@ -11,7 +12,7 @@ if (!$ssh->login("$serverUser", "$serverPW")) {
     echo " login failed ";
 } // else{ //echo "<center><h3> connected... $serverUser@$serverIP </h3></center>  <br> <pre>"; }
 
-$containers_json = $ssh->exec("sh /home/hass/web/svr01.itshass.uk/public_html/docker/site/cron/updateDownloads/run.sh");
+$containers_json = $ssh->exec("sh $site_path/site/cron/updateDownloads/run.sh");
 
 echo "<p><pre>
 $containers_json
