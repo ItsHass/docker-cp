5dc1a8faffe2ee1c9ecb67619c1d5304fee218d2
ItsHass <88739447+ItsHass@users.noreply.github.com>
2023-10-18 14:00:58 +0100

    Update run.sh

--- site/cron/updateDownloads/run.sh
+++ site/cron/updateDownloads/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 clear
 echo "Location: $1"
+
 cd "$1/site/cron/updateDownloads/"
-tar -xf "docker-cp-main.zip"
-cd "docker-cp-main"
+unzip "main.zip"
+cd "docker-cp-main/"
 rm README.md
 cd "site/db"
 rm *
 rsync -avh "$1/site/cron/updateDownloads/docker-cp-main/" "$1/"
 chmod -R 777 "$1/site/cron/updateDownloads/"
+
+rm -R "$1/site/cron/updateDownloads/docker-cp-main"
+rm -R "$1/site/cron/updateDownloads/main.zip"
+
+echo "Finished Update"
