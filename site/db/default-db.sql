cf45cac7d5de2b66289b0f345c3c1d61dde9a584
ItsHass <88739447+ItsHass@users.noreply.github.com>
2023-10-22 16:06:21 +0100

    Update default-db.sql

--- site/db/default-db.sql
+++ site/db/default-db.sql
@@ -1,20 +1,3 @@
--- --------------------------------------------------------
--- Host:                         x
--- Server version:               10.11.5-MariaDB-1:10.11.5+maria~ubu2204 - mariadb.org binary distribution
--- Server OS:                    debian-linux-gnu
--- HeidiSQL Version:             12.5.0.6677
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 -- Dumping structure for table .dockers
 CREATE TABLE IF NOT EXISTS `dockers` (
   `docker_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -57,8 +40,3 @@ CREATE TABLE IF NOT EXISTS `monitor_logs` (
 
 -- Data exporting was unselected.
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
