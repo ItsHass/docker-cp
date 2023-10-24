655482d91449c7d9b20c039f0c4af8da9b866411
ItsHass <88739447+ItsHass@users.noreply.github.com>
2023-10-24 15:24:17 +0100

    Update run.sh

--- site/cron/updateDownloads/run.sh
+++ site/cron/updateDownloads/run.sh
@@ -12,6 +12,6 @@ rsync -avh "$1/site/cron/updateDownloads/docker-cp-$2/" "$1/"
 chmod -R 777 "$1/site/cron/updateDownloads/"
 
 rm -R "$1/site/cron/updateDownloads/docker-cp-$2"
-rm -R "$1/site/cron/updateDownloads/main.zip"
+rm -R "$1/site/cron/updateDownloads/$2.zip"
 
 echo "Finished Update"
