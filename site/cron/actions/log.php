1f67c702fb750b87e83bce6e14b6b7b15676658e
ItsHass <88739447+ItsHass@users.noreply.github.com>
2023-10-24 14:08:13 +0100

    Update log.php

--- site/cron/actions/log.php
+++ site/cron/actions/log.php
@@ -1,4 +1,4 @@
 <?php
-$Insert_query = "INSERT INTO `monitor_logs` (`unixstamp`, `docker_id`, `monitor_id`, `current_status`, `pref_status`, `actions`, `json`) VALUES ('".time()."', '$id', '$mid', '$containerStatus', '$prefStatus', '$Actions_required', '$Actions_JSON');";
+$Insert_query = "INSERT INTO `monitor_logs` (`unixstamp`, `docker_id`, `monitor_id`, `current_status`, `pref_status`, `actions`, `json`) VALUES ('".time()."', '$id', '$mid', '$containerStatus', '$prefStatus', '$Actions_required', '".json_encode($Actions_JSON)."');";
 $Insert_result = mysqli_query($con, $Insert_query);
 ?>
